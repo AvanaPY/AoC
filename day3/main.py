e15878535c7619a4192d3868968f51bb23bb1ef8
Emil Karlström <emkl@WIN10-EMILK.localdomain>
2022-12-04 16:21:09 +0100

    day 4

---
+++ day3/main.py
@@ -0,0 +1,25 @@
+# https://adventofcode.com/2022/day/3
+p = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def rd():
+    fn = 'day3/data.txt'
+    with open(fn, 'r') as f:
+        data = [s.replace("\n", "") for s in f.readlines()]
+    return data
+
+def pr(a):
+    return p.index(a) + 1
+
+def c(a, b):
+    return p.index(list(set(a).intersection(b))[0]) + 1
+
+def c2(r1, r2, r3):
+    s = list(set(r1).intersection(r2, r3))[0]
+    return p.index(s) + 1
+
+d = rd()
+ss = sum([c(r[:len(r)//2], r[len(r)//2:]) for r in d])
+print(f'Answer 1: {ss}')
+
+d2 = [*(d[i:i+3] for i in range(0, len(d), 3))]
+ss = sum([c2(*a) for a in d2])
+print(f'Answer 2: {ss}')
---
+++ day4/main.py
@@ -0,0 +1,41 @@
+# https://adventofcode.com/2022/day/4
+def rd():
+    fn = 'day4/data.txt'
+    with open(fn, 'r') as f:
+        data = [s.replace("\n", "") for s in f.readlines()]
+    return data
+
+def c(p1, p2):
+    if p1[0] == p2[0]:
+        return 1
+    if p2[1] < p1[0]:
+        return 0
+    
+    if p1[0] <= p2[0] and p1[1] >= p2[1]:
+        return 1
+    if p2[0] <= p1[0] and p2[1] >= p1[1]:
+        return 1
+    return 0
+
+def c2(p1, p2):
+    if p1[1] < p2[0]:
+        return 0
+    if p2[1] < p1[0]:
+        return 0
+    return 1
+
+d = rd()
+d = [r.split(',') for r in d]
+dd = []
+for p1, p2 in d:
+    p1, p2 = p1.split('-'), p2.split('-')
+    dd.append(((int(p1[0]), int(p1[1])), (int(p2[0]), int(p2[1]))))
+
+d = dd
+s = [c(*r) for r in d]
+ss = sum(s)
+print(f'Answer 1: {ss}')
+
+s2 = [c2(*r) for r in d]
+ss2 = sum(s2)
+print(f'Answer 2: {ss2}')
