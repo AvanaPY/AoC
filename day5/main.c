6649f08f9a7ab3a7702a6e250603ecb3c4b8674b
Emil Karlström <emkl@WIN10-EMILK.localdomain>
2022-12-06 11:19:42 +0100

    this just gets more cursed

--- day5/main.c
+++ day5/main.c
@@ -15,7 +15,6 @@ FILE* open_data()
 int main(void)
 {   
     char c;
-    int ans1 = 0, ans2 = 0;
     FILE* f = open_data();
     if(f == NULL){
         printf("Could not open file\n");
@@ -23,17 +22,23 @@ int main(void)
     }
     /* Write code between here */
 
+    /* Stack and state variables */
     int state_counter = 0;
     char state[4096] = { ' ' }; /* Init a buffer of 4MB */
-    char stack[MAX_STACK_SZ * MAX_STACKS] = { '\n' };
-    char stack2[MAX_STACK_SZ * MAX_STACKS] = { '\n' };
+    char stack[MAX_STACK_SZ * MAX_STACKS * 2] = { '\n' };
     int stack_size[MAX_STACKS] = { 0 };
     int stacks_total = 0;
 
+    /* Reading state variables*/
+    int ii, jj, bi, bj;
     int hit_end_of_init = 0;
     int init_state_text_width = 0;
     int init_state_stack_sz = 0;
     
+    /* Execute instructions variables */
+    int from, targ, from_idx, targ_idx, i;
+    char b;
+
     do
     {
         c = fgetc(f);
@@ -44,17 +49,17 @@ int main(void)
                 hit_end_of_init = 1;
 
                 /* Transform read stack into our internal stack */
-                for(int i = 0; i < stacks_total; i++)
+                for(ii = 0; ii < stacks_total; ii++)
                 {
-                    int bj = 4 * i + 1;
-                    for(int j = 0; j < init_state_stack_sz - 1; j++)
+                    bj = 4 * ii + 1;
+                    for(jj = 0; jj < init_state_stack_sz - 1; jj++)
                     {
-                        int bi = init_state_stack_sz - 2 - j;
+                        bi = init_state_stack_sz - 2 - jj;
                         c = state[bi * (init_state_text_width + 1) + bj];
-                        stack[i * MAX_STACK_SZ + j] = c;
-                        stack2[i * MAX_STACK_SZ + j] = c;
+                        stack[ii * MAX_STACK_SZ + jj] = c;
+                        stack[ii * MAX_STACK_SZ + jj + MAX_STACK_SZ * MAX_STACKS] = c;
                         if(c != ' ')
-                            stack_size[i]++;
+                            stack_size[ii]++;
                     }
                 }
             }
@@ -99,27 +104,28 @@ int main(void)
                 else if(('0' <= c && c <= '9'))
                     state[state_counter++] = c;
             } while(c != EOF);
-            state[state_counter] = '\0';
-
+            
             /* Debuging takes too long */
-            int from = vals[1] - 1;
-            int targ = vals[2] - 1;
+            from = vals[1] - 1;
+            targ = vals[2] - 1;
             if(stack_size[from] < vals[0])
                 exit(1);
                 
-            for(int i = 0; i < vals[0]; i++)
+            for(i = 0; i < vals[0]; i++)
             {
-                int from_idx = from * MAX_STACK_SZ + stack_size[from] - 1 - i;
-                int targ_idx = targ * MAX_STACK_SZ + stack_size[targ] + (vals[0] - i) - 1;
-                char b = stack2[from_idx];
-                stack2[targ_idx] = b;
-                stack2[from_idx] = ' ';
+                from_idx = from * MAX_STACK_SZ + (stack_size[from] - 1 - i) + MAX_STACK_SZ * MAX_STACKS;
+                targ_idx = targ * MAX_STACK_SZ + (stack_size[targ] + (vals[0] - i) - 1) + MAX_STACK_SZ * MAX_STACKS;
+                b = stack[from_idx];
+
+                stack[targ_idx] = b;
+                stack[from_idx] = ' ';
             }
-            for(int i = 0; i < vals[0]; i++)
+            for(i = 0; i < vals[0]; i++)
             {
-                int from_idx = from * MAX_STACK_SZ + stack_size[from] - 1;
-                int targ_idx = targ * MAX_STACK_SZ + stack_size[targ];
-                char b = stack[from_idx];
+                from_idx = from * MAX_STACK_SZ + stack_size[from] - 1;
+                targ_idx = targ * MAX_STACK_SZ + stack_size[targ];
+                b = stack[from_idx];
+
                 stack[targ_idx] = b;
                 stack[from_idx] = ' ';
                 stack_size[from]--;
@@ -128,7 +134,6 @@ int main(void)
         }
         /* code */
     } while (c != EOF);
-    /* and here */
     
     printf("Answer 1: ");
     for(int i = 0; i < stacks_total; i++)
@@ -136,7 +141,7 @@ int main(void)
     printf("\n");
     printf("Answer 2: ");
     for(int i = 0; i < stacks_total; i++)
-        printf("%c", stack2[i * MAX_STACK_SZ + stack_size[i] - 1]);
+        printf("%c", stack[i * MAX_STACK_SZ + MAX_STACK_SZ * MAX_STACKS + stack_size[i] - 1]);
     printf("\n");
     fclose(f);
     return 0;
